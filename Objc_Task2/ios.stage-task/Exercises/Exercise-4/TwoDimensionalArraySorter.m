673df9a061dbc9c24b026cc22f0b72b5841b4481
Olga Stash <olya.stash@yandex.ru>
2021-05-18 01:54:44 +0300

    fix: add ex 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,29 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (array == nil || array.count == 0 || ![array[0] isKindOfClass:NSArray.class]){
+           return @[];
+    }
+    NSMutableArray<NSNumber *> *numbersSubarray = [NSMutableArray new];
+    NSMutableArray<NSString *> *charactersSubarray = [NSMutableArray new];
+    NSArray *subarray = array;
+    for (subarray in array){
+        for (id element in subarray){
+            if ([element isKindOfClass:NSNumber.class])
+                [numbersSubarray addObject:element];
+            else
+                [charactersSubarray addObject:element];
+        }
+    }
+    if (charactersSubarray.count == 0) {
+        return [numbersSubarray sortedArrayUsingSelector:@selector(compare:)];
+    }
+    
+    if (numbersSubarray.count == 0) {
+        return [charactersSubarray sortedArrayUsingSelector:@selector(compare:)];
+    }
+        return @[[numbersSubarray sortedArrayUsingSelector:@selector(compare:)],
+                 [charactersSubarray sortedArrayUsingSelector:@selector(compare:)]];
 }
 
 @end
